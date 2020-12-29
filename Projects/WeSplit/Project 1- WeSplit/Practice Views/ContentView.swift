e21f0ad1f12c33b4c8986fa4ccb1c3bedc62dabf
Ang Jun Ray <angjunray@live.com>
2020-12-29 20:05:30 +0800

    Day 1 of projects done! Sorted stuff out too :D

--- Projects/WeSplit/Project 1- WeSplit/Practice Views/ContentView.swift
+++ Projects/WeSplit/Project 1- WeSplit/Practice Views/ContentView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    @State private var name = ""
     @State var tapcount = 0 //@state allows swift to give ur code permission to modify this variable since it manages states
     var body: some View {
+        Picker("Select your student", selection: $selectedStudent){
+            ForEach(0 ..< students.count){
+                Text(self.students[$0])
+            }
+        }
+        Form{
+            ForEach(0..<100){
+                Text("Row \($0)")
+            }
+        }
+        /*
+        Form{
+            TextField("Enter your name", text:$name)//$ tells swift to read the value of property and write it back so nothing should happen
+
+            Text("Your name is \(name)") //no 2 way binding, cuz no reading it back
+        }
+ */
+        /*
         Button("Tap Count \(tapcount)"){
             self.tapcount += 1
         }
+ */
         /*
         NavigationView{
             Form{
