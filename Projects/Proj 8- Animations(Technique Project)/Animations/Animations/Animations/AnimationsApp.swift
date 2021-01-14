8760974e0a1756e25cebf03e80d80be7a80135ff
Ang Jun Ray <angjunray@live.com>
2021-01-14 17:33:13 +0800

    Animation App FIrst day cool

---
+++ Projects/Proj 8- Animations(Technique Project)/Animations/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Ang Jun Ray on 14/1/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
