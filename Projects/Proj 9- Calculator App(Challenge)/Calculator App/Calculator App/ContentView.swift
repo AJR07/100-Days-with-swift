6e0f869d6115791d0c4f57afa3426d0a33e6ba8e
Ang Jun Ray <angjunray@live.com>
2021-01-18 21:58:53 +0800

    Proj 9

---
+++ Projects/Proj 9- Calculator App(Challenge)/Calculator App/Calculator App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator App
+//
+//  Created by Ang Jun Ray on 17/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
