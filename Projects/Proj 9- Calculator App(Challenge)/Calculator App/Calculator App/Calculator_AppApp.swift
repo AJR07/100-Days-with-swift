18f8c3de2d539ee1b21050d6a3528e7b82bd4371
Ang Jun Ray <angjunray@live.com>
2021-01-19 19:16:04 +0800

    Recommitting due to file error

---
+++ Projects/Proj 9- Calculator App(Challenge)/Calculator App/Calculator App/Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_AppApp.swift
+//  Calculator App
+//
+//  Created by Ang Jun Ray on 17/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
