7cfca4e8320152c463afd518e307031df9b771f6
Ang Jun Ray <angjunray@live.com>
2021-01-24 13:35:48 +0800

    Removing sample code

---
+++ Projects/Proj 11- Moonshot/MoonShot/MoonShot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoonShot
+//
+//  Created by Ang Jun Ray on 24/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Projects/Proj 11- Moonshot/MoonShot/MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Ang Jun Ray on 24/1/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
