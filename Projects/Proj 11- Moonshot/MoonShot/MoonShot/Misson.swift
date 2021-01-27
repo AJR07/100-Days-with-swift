f139c428629ae582a48c25d6f905efa18ef745f3
Ang Jun Ray <angjunray@live.com>
2021-01-27 21:10:18 +0800

    test

--- Projects/Proj 11- Moonshot/MoonShot/MoonShot/Misson.swift
+++ Projects/Proj 11- Moonshot/MoonShot/MoonShot/Misson.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+
+
+
+struct Mission: Codable, Identifiable{
+    struct CrewRoles: Codable{
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRoles]
+    let description: String
+    
+    var displayName: String{
+        "Apollo \(id)"
+    }
+    
+    var image: String{
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String{
+        if let launchDate = launchDate{
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            return formatter.string(from: launchDate)
+        }else{
+            return "N/A"
+        }
+    }
+}
