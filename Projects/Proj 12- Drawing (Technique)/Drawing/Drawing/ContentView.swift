96a80d4fced908b394f15edf5a239042e7649bb1
Ang Jun Ray <angjunray@live.com>
2021-02-16 17:24:45 +0800

    Proj 12 started! Day 43 done!

---
+++ Projects/Proj 12- Drawing (Technique)/Drawing/Drawing/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Ang Jun Ray on 16/2/21.
+//
+
+import SwiftUI
+
+struct Traingle: Shape{
+    func path(in rect: CGRect) -> Path{
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        return path
+    }
+}
+
+struct Arc: InsettableShape{
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    var insertAmount: CGFloat = 0
+    
+    func path(in rect: CGRect) -> Path{
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+        
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width/2 - insertAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape{
+        var arc = self
+        arc.insertAmount += amount
+        return arc
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        /*
+        Path{ path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+        }
+        .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+ */
+        /*
+        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+            .stroke(Color.blue, lineWidth: 10)
+            .frame(width: 300, height: 300)
+ */
+        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+            .strokeBorder(Color.blue, lineWidth: 40)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Projects/Proj 12- Drawing (Technique)/Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Ang Jun Ray on 16/2/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
